9863e6cf94732b0cd11d13d5df0ca8ed070061f0
Jordan Bangia <jordan.bangia@mail.utoronto.ca>
2013-09-04 16:17:50 -0400

    Pulls list of files from all subdirectories in specified path

--- MusicDiff/Form1.Designer.cs
+++ MusicDiff/Form1.Designer.cs
@@ -29,11 +29,40 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Default;
+            this.listView1.Location = new System.Drawing.Point(118, 86);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(680, 394);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 647);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
--- MusicDiff/Form1.cs
+++ MusicDiff/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,19 @@ namespace MusicDiff
         public Form1()
         {
             InitializeComponent();
+            listView1.Items.Clear();
+            listView1.View = View.Details;
+            listView1.Columns.Add("Path");
+            listView1.Columns[0].Width = listView1.Width - 4;
+            listView1.HeaderStyle = ColumnHeaderStyle.None;
+
+            string dir = "C:\\Users\\Jordan\\Desktop\\MusicDiff-Test\\";
+            List<string> dirs = new List<string>(Directory.EnumerateFiles(dir, "*.*", SearchOption.AllDirectories));
+            foreach (var subDir in dirs)
+            {
+                listView1.Items.Add(new ListViewItem(subDir.Replace(dir, "")));
+            }
+            Console.WriteLine("{0} directories found.", dirs.Count);
         }
     }
 }
