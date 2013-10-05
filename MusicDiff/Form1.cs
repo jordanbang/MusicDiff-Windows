4c47297c77968da3002faeaf298182efa7b8650d
Jordan Bangia <jordan.bangia@mail.utoronto.ca>
2013-10-05 17:20:28 -0400

    Now reads from itunes folder, and displays outputs to console

--- MusicDiff/Form1.cs
+++ MusicDiff/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,17 +23,30 @@ namespace MusicDiff
             listView1.Columns[0].Width = listView1.Width - 4;
             listView1.HeaderStyle = ColumnHeaderStyle.None;
 
-            string dir = "C:\\Users\\Jordan\\Desktop\\MusicDiff-Test\\";
+            string dir = "D:\\iTunes Music\\Music";
+            //dir = "C:\\Users\\Jordan\\Desktop\\MusicDiff-Test";
             List<System.IO.FileInfo> files = new List<FileInfo>();
             List<string> dirs = new List<string>(Directory.EnumerateFiles(dir, "*.*", SearchOption.AllDirectories));
             foreach (var subDir in dirs)
             {
                 FileInfo file = new FileInfo(subDir);
                 TagLib.File sameFile = TagLib.File.Create(subDir);
-
-                listView1.Items.Add(new ListViewItem("Title: " + sameFile.Tag.Title + "  -  Artist: " + sameFile.Tag.Performers[0]));
+                Console.WriteLine(subDir);
+                int length = sameFile.Tag.Performers.Length;
+                listView1.Items.Add(new ListViewItem("Title: " + sameFile.Tag.Title + "  -  Artist: " + (length > 0 ? sameFile.Tag.Performers[0] : "")));
             }
-            Console.WriteLine("{0} directories found.", dirs.Count);
+            Console.WriteLine("\n{0} directories found.", dirs.Count);
+
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("HttpListener is not supported");
+                return;
+            }
+
+            //HttpListener listener = new HttpListener();
+            //listener.Start();
+            //Console.WriteLine("Listening..");
+            ////listener.
         }
 
         private void Form1_Load(object sender, EventArgs e)
--- MusicDiff/Program.cs
+++ MusicDiff/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,11 +12,16 @@ namespace MusicDiff
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            Console.WriteLine("MusicDiff Server - by jBigBangs");
+
             Application.Run(new Form1());
         }
     }
